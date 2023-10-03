2465e36af56c4d55f3d81b35daf1a32004a71eb7
TanayDesai <tanaydesai5.com>
2023-10-04 03:46:44 +0530

    V2.0.1

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ import Link from "next/link"
 export default function Home() {
 
   const projects = [
-      {title: "explore ai", description: "an AI Powered Search for Youtube Videos. Enables search for answers directly inside thous&s of YouTube videos, free-of-cost, easy-to-navigate & fast.",tag:"31.12.2022",links:["https://exploreai.vercel.app/"]},
+      {title: "explore ai", description: "an AI Powered Search for Youtube Videos. Enables search for answers directly inside thousands of YouTube videos, free-of-cost, easy-to-navigate & fast.",tag:"31.12.2022",links:["https://exploreai.vercel.app/"]},
       {title: "uiaftersex", description: "an open-source intuitive laboratory that showcases uniquely crafted user interfaces, interactions & components that make you wanna (hopefully) come back to, after sex.",tag:"31.12.2022",links:["https://uiaftersex.vercel.app/","https://github.com/tanaydesai/uiaftersex"]}
 ]
 
@@ -22,7 +22,7 @@ export default function Home() {
       <p className='description'>18, a software alchemist by day & user interfaces & interactions craftsman by night. Into deep learning & neural nets & sometimes making other intiutive & puposeful software.</p>
       <div className='icons'><Link href="https://twitter.com/tanaydesaii"><FaXTwitter /></Link> <Link href="https://github.com/tanaydesai"><BsGithub /></Link></div>
 
-      <div className='mt-[200px] md:mt-[150px]'>
+      <div className='mt-[200px] md:mt-[200px]'>
         <div className='header'><BsCodeSlash /> Projects</div>
           {projects.map((project) => (
             <Slab data={project} type="projects"/>
