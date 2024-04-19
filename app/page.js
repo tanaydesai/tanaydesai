eabf8ebc2560627260cf66173a70e5559f8342d8
TanayDesai <tanaydesai5.com>
2024-04-20 02:11:25 +0530

    Site:) 3.0

--- app/page.js
+++ app/page.js
@@ -20,8 +20,8 @@ const Home = () => {
                 <div className="tag mb-10"><DecryptText text="Mumbai, India."/></div>
                 <FadeBox sentence={`I am a 19 year old developer focused on deep learning and neural networks, with a keen interest in user interface and interaction design. My overarching goal is to build towards the democratization of LLMs and help catalyze progress towards an age of abundance. \n \n I firmly believe in empowering individuals to harness the intelligence and creativity facilitated by generative models, enabling the creation of remarkable tools. \n \n Currently building a venture focused on embedding large text corpora.`}/>
                 <div className="flex gap-5 items-center text-gray-500 ml-2  mt-10">
-                    <Link href="https://twitter.com/tanaydesaii" className="border-2 border-grey-600 px-2 py-1 rounded-full flex gap-1 items-center justify-center text-[11px]"><div className="w-3 h-3 p-1/3 rounded-full flex items-center justify-center bg-[#d2a999]/50"><div className="w-1/2 h-1/2 shrink-0 rounded-full bg-[#d2a999]"/></div> github ↗</Link>
-                    <Link href="https://github.com/tanaydesai" className="border-2 border-grey-600 px-2 py-1 rounded-full flex gap-1 items-center justify-center text-[11px]"><div className="w-3 h-3 p-1/3 rounded-full flex items-center justify-center bg-[#d2a999]/50"><div className="w-1/2 h-1/2 shrink-0  rounded-full bg-[#d2a999]"/></div> twitter ↗</Link>
+                    <Link href="https://twitter.com/tanaydesaii" className="border-2 border-grey-600 px-2 py-1 rounded-full flex gap-1 items-center justify-center text-[11px]"><div className="w-3 h-3 p-1/3 rounded-full flex items-center justify-center bg-[#d2a999]/50"><div className="w-1/2 h-1/2 shrink-0 rounded-full bg-[#d2a999]"/></div> github &#8599;</Link>
+                    <Link href="https://github.com/tanaydesai" className="border-2 border-grey-600 px-2 py-1 rounded-full flex gap-1 items-center justify-center text-[11px]"><div className="w-3 h-3 p-1/3 rounded-full flex items-center justify-center bg-[#d2a999]/50"><div className="w-1/2 h-1/2 shrink-0  rounded-full bg-[#d2a999]"/></div> twitter &#8599;</Link>
                 </div>
             </div>
         </div>
@@ -37,13 +37,13 @@ const Home = () => {
                 <div className="mt-3">
                   <Link href="https://uiaftersex.vercel.app/" >
                     <div className="p-3 cursor-default rounded-lg hover:bg-gray-200">
-                      <h1 className="mb-2 font-STY text-[15px] flex items-center gap-2.5 justify-between"><p>uiaftersex ↗</p><DecryptText text="2023"/></h1>
+                      <h1 className="mb-2 font-STY text-[15px] flex items-center gap-2.5 justify-between"><p>uiaftersex &#8599;</p><DecryptText text="2023"/></h1>
                       <h1 className="text-[#7f7f7d] text-[14px] font-STY">Laboratory for uniquely crafted user interfaces, interactions & components.</h1>
                     </div>
                   </Link>
                   <Link href="https://exploreai.vercel.app/">
                     <div className="p-3 cursor-default rounded-lg hover:bg-gray-200">
-                      <h1 className="mb-2 font-STY  text-[15px] flex items-center gap-2.5 justify-between"><p>Explore AI ↗</p><DecryptText text="2023"/></h1>
+                      <h1 className="mb-2 font-STY  text-[15px] flex items-center gap-2.5 justify-between"><p>Explore AI &#8599;</p><DecryptText text="2023"/></h1>
                       <h1 className="text-[#7f7f7d] font-STY text-[14px]">AI powered semantic search engine for thousands of Youtube videos.</h1>
                     </div>
                   </Link>
@@ -51,8 +51,8 @@ const Home = () => {
 
                 <h1 className=" text-[20px] mt-10 ml-2 font-STY">Open Source</h1>
                 <Link href=""><div className="text-[#91918d] ml-2 font-STY text-[14px] mt-5 cursor-default  hover:underline hover:decoration-[#7f7f7d]"><div className="text-[#7f7f7d] text-[14px] font-STY">scaling-laws - wip:)</div></div></Link>
-                <Link href="https://github.com/tanaydesai/pluto"><div className=" ml-2 text-[14px] mt-2 cursor-default font-STY hover:underline hover:decoration-[#7f7f7d]"><div className="text-[#7f7f7d] text-[14px] font-STY">pluto - build small language models with synthetic data from scratch ↗</div></div></Link>
-                <Link href="https://github.com/tanaydesai/atomgrad"><div className=" ml-2 text-[14px] mt-2 cursor-default font-STY hover:underline hover:decoration-[#7f7f7d]"><div className="text-[#7f7f7d] text-[14px] font-STY">atomgrad - simple autograd engine between micrograd and tinygrad ↗</div></div></Link>
+                <Link href="https://github.com/tanaydesai/pluto"><div className=" ml-2 text-[14px] mt-2 cursor-default font-STY hover:underline hover:decoration-[#7f7f7d]"><div className="text-[#7f7f7d] text-[14px] font-STY">pluto - build small language models with synthetic data from scratch &#8599;</div></div></Link>
+                <Link href="https://github.com/tanaydesai/atomgrad"><div className=" ml-2 text-[14px] mt-2 cursor-default font-STY hover:underline hover:decoration-[#7f7f7d]"><div className="text-[#7f7f7d] text-[14px] font-STY">atomgrad - simple autograd engine between micrograd and tinygrad &#8599;</div></div></Link>
             </div>
           </div>
         </TabsContent>
